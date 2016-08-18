edc453eb584d7339b96b9c197f28056449c07293
matthes <leuffen@continue.de>
2016-08-18 13:39:15 +0200

    FHtml added

--- src/Template/FHtml.php
+++ src/Template/FHtml.php
@@ -31,7 +31,7 @@
         private $curNode;
         private $jumpMarks = [];
 
-        private $emptyTags = ["meta"=>true, "img"=>true, "br"=>true, "hr"=>true];
+        private $emptyTags = ["meta"=>true, "img"=>true, "br"=>true, "hr"=>true, "input"=>true];
 
         public function __construct(HtmlTemplate $bindParser=null) {
             if ($bindParser === null)
